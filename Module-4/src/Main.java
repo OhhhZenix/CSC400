5912aa907a49149a126afa4290e7a2bfdfca13ca
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2024-09-08 13:06:12 -0600

    Update Main.java

--- Module-4/src/Main.java
+++ Module-4/src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
 
   public static void main(String[] args) {
+    // Create a calculator instance
     InfixCalculator calculator = new InfixCalculator();
+    // Sample testing
     calculator.compute("(4 + 2) * 3");
     calculator.compute("5 + (3 * 7)");
     calculator.compute("4 + 2 * 3");
-
     calculator.compute("1 + 2 * (3 + (4 * 5) + 6)");
     calculator.compute("10 / 0");
     calculator.compute("2x + 2");
