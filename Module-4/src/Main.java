be714c1ded5f61ae8c1aa996df81d09c3451cbbc
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2024-09-08 12:52:53 -0600

    Update Main.java

--- Module-4/src/Main.java
+++ Module-4/src/Main.java
@@ -2,24 +2,17 @@ public class Main {
 
   public static void main(String[] args) {
     InfixCalculator calculator = new InfixCalculator();
-
     calculator.compute("(4 + 2) * 3");
     calculator.compute("5 + (3 * 7)");
     calculator.compute("4 + 2 * 3");
 
-    // try (Scanner scanner = new Scanner(System.in)) {
-    //   while (true) {
-    //     System.out.println("Enter an infix expression or type 'q' to quit");
-    //     String input = scanner.nextLine();
-    //
-    //     if (input.equalsIgnoreCase("q")) {
-    //       break;
-    //     }
-    //
-    //     calculator.compute(input);
-    //   }
-    // }
-    //
-    // System.out.println("Bye bye now!");
+    calculator.compute("1 + 2 * (3 + (4 * 5) + 6)");
+    calculator.compute("10 / 0");
+    calculator.compute("2x + 2");
+    calculator.compute("6(7 + 8)");
+    calculator.compute("6 ++ (7 + 8)");
+    calculator.compute("2 + ( 3 * 4");
+    calculator.compute("2 + 3 * 4)");
+    calculator.compute("");
   }
 }
