bc3f2084f3565ce9705ffe980cc34a581c22fa62
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2024-09-09 14:54:11 -0600

    Update Main.java

--- Module-5/src/Main.java
+++ Module-5/src/Main.java
@@ -1,3 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
-  public static void main(String[] args) {}
+
+  public static void main(String[] args) {
+    int[] sample = {783, 99, 472, 182, 264, 543, 356, 295, 692, 491, 94};
+    RadixSorter sorter = new RadixSorter();
+    System.out.println("Before: " + Arrays.toString(sample));
+    sorter.sort(sample);
+    System.out.println("After: " + Arrays.toString(sample));
+  }
 }
