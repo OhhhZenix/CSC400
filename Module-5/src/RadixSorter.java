7b8e74e5113c2c43003801dfad6ad6b717b1d899
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2024-09-26 19:21:25 -0600

    update to work with negatives

--- Module-5/src/RadixSorter.java
+++ Module-5/src/RadixSorter.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * This class provides an implementation of the Radix Sort algorithm for sorting an array of
  * non-negative integers. Radix Sort is a non-comparative integer sorting algorithm that sorts
@@ -61,17 +63,60 @@ public class RadixSorter {
   }
 
   /**
+   * Processes the given array by finding the maximum value and performing counting sort for each
+   * digit place value.
+   *
+   * @param array The array to be processed and sorted.
+   */
+  private void processArray(int[] array) {
+    // Base case, if empty then stop.
+    if (array.length == 0) return;
+    // Find the maximum value in the array.
+    int max = getMaxValue(array);
+    // Perform counting sort for each digit place value.
+    for (int exp = 1; max / exp > 0; exp *= 10) {
+      countSort(array, exp);
+    }
+  }
+
+  /**
    * Sorts the given array using Radix Sort.
    *
    * @param array The array to be sorted.
    */
   public void sort(int[] array) {
-    // Find the maximum value in the array.
-    int max = getMaxValue(array);
+    // List to hold negatives and positives.
+    ArrayList<Integer> negatives = new ArrayList<>();
+    ArrayList<Integer> positives = new ArrayList<>();
 
-    // Perform counting sort for each digit place value.
-    for (int exp = 1; max / exp > 0; exp *= 10) {
-      countSort(array, exp);
+    // Separate numbers by negatives and positives.
+    for (int num : array) {
+      if (num < 0) {
+        // Store by absolute value.
+        negatives.add(-1 * num);
+      } else {
+        // Store normally.
+        positives.add(num);
+      }
+    }
+
+    // Convert lists to arrays.
+    int[] sortedNegatives = negatives.stream().mapToInt(i -> i).toArray();
+    int[] sortedPositives = positives.stream().mapToInt(i -> i).toArray();
+
+    // Proccess and sort each arrays.
+    processArray(sortedNegatives);
+    processArray(sortedPositives);
+
+    // Include sorted negatives to output.
+    for (int i = 0; i < sortedNegatives.length; i++) {
+      // Reverse order for negatives
+      array[i] = -sortedNegatives[sortedNegatives.length - 1 - i];
+    }
+
+    // Include sorted positives to output.
+    for (int i = 0; i < sortedPositives.length; i++) {
+      array[sortedNegatives.length + i] = sortedPositives[i];
     }
   }
 }
