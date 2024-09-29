6d662f015f13f127d85b341f2761d278b65e53fb
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2024-09-28 19:13:25 -0600

    Create LastNameComparator.java

---
+++ Module-8/src/LastNameComparator.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class LastNameComparator implements Comparator<Person> {
+
+  @Override
+  public int compare(Person a, Person b) {
+    return a.getLastName().compareTo(b.getLastName());
+  }
+}
